8415fd31c217b00d0d5d82f10cd0948fccbb3bf9
John Ting <john.ting@salesforce.com>
2022-02-16 13:29:21 -0300

    RunAsTest apex class added

---
+++ force-app/main/default/lwc/bike/bike.js
@@ -0,0 +1,10 @@
+import { LightningElement, api } from 'lwc';
+export default class Bike extends LightningElement {
+    @api bike;
+    ready = false;
+    connectedCallback() {
+        setTimeout(() => {
+            this.ready = true;
+        }, 3000);
+    }
+}
